423c06e1584dc0589dd750eeffa450a0588dad33
jarvis-sharma <ankitsharma072001@gmail.com>
2020-09-17 03:01:30 +0530

    shifted the calculator to the center

--- calculator.js
+++ calculator.js
@@ -5,12 +5,12 @@ var screen = document.querySelector(".screen");
 
 var value = document.querySelector(".calc-buttons");
 
-var actualValue = value.addEventListener("click", e => {
+var actualValue = value.addEventListener("click", (e) => {
   buttonValue(e.target.innerText);
 });
 
 function buttonValue(actualValue) {
-  if (isNaN(parseInt(actualValue))){
+  if (isNaN(parseInt(actualValue))) {
     handleSymbol(actualValue);
   } else {
     handleNumber(actualValue);
@@ -27,65 +27,62 @@ function handleNumber(value) {
 }
 
 function handleSymbol(value) {
-    switch(value){
-        case 'C' :
-        buffer = '0';
-        runningTotal = 0;
-        previousOperator = null;
-        break;
+  switch (value) {
+    case "C":
+      buffer = "0";
+      runningTotal = 0;
+      previousOperator = null;
+      break;
 
-        case '=' : 
-        if(previousOperator === null){
-            return
-        }
-        
-        flushOperation(parseInt(buffer));
-        previousOperator = null;
-        buffer = '' + runningTotal;
-        runningTotal = 0 ;
-        break;
+    case "=":
+      if (previousOperator === null) {
+        return;
+      }
 
-        case '←' :
-        if(buffer.length === 1){
-            buffer = '0';
-        }else{
-            buffer = buffer.substring(0,buffer.length-1);
-        }
-        break;
+      flushOperation(parseInt(buffer));
+      previousOperator = null;
+      buffer = "" + runningTotal;
+      runningTotal = 0;
+      break;
 
-        default:
-        handleMath(value);
-        break;
-    }
+    case "←":
+      if (buffer.length === 1) {
+        buffer = "0";
+      } else {
+        buffer = buffer.substring(0, buffer.length - 1);
+      }
+      break;
+
+    default:
+      handleMath(value);
+      break;
+  }
 }
 
+function handleMath(value) {
+  const intbuffer = parseInt(buffer);
+  if (runningTotal === 0) {
+    runningTotal = intbuffer;
+  } else {
+    flushOperation(intbuffer);
+  }
+  previousOperator = value;
 
-function handleMath(value){
-    const intbuffer = parseInt(buffer);
-    if(runningTotal === 0 ){
-        runningTotal = intbuffer;
-    }else{
-        flushOperation(intbuffer);
-    }
-    previousOperator = value;
-
-    buffer = '0';
+  buffer = "0";
 }
 
-function flushOperation(intbuffer){
-    if(previousOperator === '+'){
-        runningTotal += intbuffer
-    } else if(previousOperator === '-'){
-        runningTotal -= intbuffer
-    } else if(previousOperator === 'ˣ'){
-        runningTotal *= intbuffer
-    } else {
-        runningTotal /= intbuffer
-    }
-    
+function flushOperation(intbuffer) {
+  if (previousOperator === "+") {
+    runningTotal += intbuffer;
+  } else if (previousOperator === "-") {
+    runningTotal -= intbuffer;
+  } else if (previousOperator === "ˣ") {
+    runningTotal *= intbuffer;
+  } else {
+    runningTotal /= intbuffer;
+  }
 }
 
 function rerender() {
-    screen.innerText = buffer;
-  }
-  
+  screen.innerText = buffer;
+}
